624ac230762a8cb143bfb95c69d979a16d88e031
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-22 21:47:39 +0000

    Automatically backed up by Learn

---
+++ app/controllers/attractions_controller.rb
@@ -0,0 +1,37 @@
+class AttractionsController < ApplicationController 
+
+  def index 
+    @attractions = Attraction.all 
+    @user = current_user 
+  end 
+  
+  def show 
+    @attraction = Attraction.find(params[:id])
+    @user = current_user
+  end 
+  
+  def create
+    @attraction = Attraction.create(attraction_params)
+    if @attraction
+      redirect_to attraction_path(@attraction)
+    else
+      render :new
+    end
+  end
+  
+  def update
+    @attraction.update(attraction_params)
+    if @attraction.save
+      redirect_to attraction_path(@attraction)
+    else
+      render :edit
+    end
+  end
+  
+  private 
+  
+  def attraction_params
+    params.require(:attraction).permit(:name, :min_height, :tickets, :hapiness_rating, :nausea_rating)
+  end 
+
+end 
